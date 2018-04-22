7db1b820585396abbb0bd6a1dac1036b63b97029
lanyage <lanyage520@foxmail.com>
2018-04-22 10:21:36 +0800

    add timeformater

--- js/home.js
+++ js/home.js
@@ -858,6 +858,7 @@ var home = {
      * @hides 代表所有三级菜单点击后显示的内容的id
      */
     , init: function (userJson, menu1Wrapper, menu2Wrapper) {
+        home.funcs.updateDatePrototype()
         /** 头像显示事件绑定 */
         home.funcs.bindClickEventForAvatar($('#user-info-hover'), $("#hover-body"))
         home.menu1Wrapper = menu1Wrapper
@@ -930,6 +931,27 @@ var home = {
     }
     ,//$init()
     funcs: {
+        updateDatePrototype : function() {
+            Date.prototype.Format = function (fmt) { //author: meizz
+                var o = {
+                    "M+": this.getMonth() + 1, //月份
+                    "d+": this.getDate(), //日
+                    "h+": this.getHours(), //小时
+                    "m+": this.getMinutes(), //分
+                    "s+": this.getSeconds(), //秒
+                    "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+                    "S": this.getMilliseconds() //毫秒
+                };
+                if (/(y+)/.test(fmt)) {
+                    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+                }
+                for (var k in o)
+                    if (new RegExp("(" + k + ")").test(fmt))
+                        fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+                return fmt;
+
+            }
+        },
         /** 给一级菜单绑定点击事件 */
         bindClickForMenu1s: function () {
             home.menu1Clicks.on('click', function () {
