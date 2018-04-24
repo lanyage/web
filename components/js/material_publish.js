044d8f3777356f3b314710cb4b62f65b917dca33
lanyage <lanyage520@foxmail.com>
2018-04-24 16:24:45 +0800

    finish click event binding for blockquote spans
    凯凯可以在这个基础上去渲染表格数据了.

---
+++ components/js/material_publish.js
@@ -0,0 +1,20 @@
+var material_publish = {
+    init : function() {
+        material_publish.funcs.bindClickForSpanInBlockQuote($('.block-quote span'))
+
+
+        /** 显示逻辑.根据档案的表格去渲染数据 */
+        //todo
+    },
+    funcs : {
+        bindClickForSpanInBlockQuote : function(clickSpans){
+            clickSpans.off('click')
+            clickSpans.on('click', function(){
+                $('.select_span').removeClass('select_span')
+                $('#model-li-hide-24 table').addClass('hide')
+                $('.'+$(this).attr('id')+'_table').removeClass('hide')
+                $(this).addClass('select_span')
+            })
+        }
+    }
+}
