95311b9ce0d7ed42a0647b6dc07931573acbf3bc
yukaizhou <2717211213@qq.com>
2018-04-26 23:15:26 +0800

    Wanchengshujudaoru
    完成了数据导入的功能

--- components/js/inputData_manage.js
+++ components/js/inputData_manage.js
@@ -83,7 +83,7 @@ var inputData_manage = {
                 var bb = $('#tbody_page'+code)
                 console.log(bb)
                 switch(code){
-                    case 1: a = 'p';
+                    case 1: inputData_manage.funcs.bindProduct(returndata,bb);
                     break;
                     case 2: inputData_manage.funcs.bindJinchi622(returndata,bb) ;
                     break;
@@ -93,9 +93,11 @@ var inputData_manage = {
                     break;
                     case 5: a = 'pc';
                     break;
-                    case 6: a = 'pc';
+                    case 6: inputData_manage.funcs.bindZhiChengLidu(returndata,bb);
                     break;
-                    case 7: a = 'pc';
+                    case 7: inputData_manage.funcs.bindZhiChengZongli(returndata,bb);
+                    break;
+                    case 8: inputData_manage.funcs.bindZhiChengKoudian(returndata,bb);
                     break;
                     default:
                     console.log('没有和以上的匹配上！！！')
@@ -103,14 +105,17 @@ var inputData_manage = {
             },
             error:function(returndata,Ccode){
                 console.log(returndata)
+                for(var i=0;i<length;i++){
+
+                }
             }
         });
 
     })
          },
 
-        /** 天齐碳酸锂添加Tbody */
-        bindTianQi:function(result,Tcode){
+         /** 制程扣电添加Tbody */
+         bindZhiChengKoudian:function(result,Tcode){
             var length = result.data['length']
             console.log(length)
             for(var i=0;i<length;i++){
@@ -118,59 +123,118 @@ var inputData_manage = {
                 var publisher = result.data[i]['publisher']
                 var auditDate = result.data[i]['auditDate']
                 var batchNumber = result.data[i]['batchNumber']
-                var productDate = result.data[i]['productDate'] 
-                var judge = result.data[i]['judge']?result.data[i]['judge']['name']:'null'
-                var number = result.data[i]['number'] 
-                var c1 = result.data[i]['c1']//水分
-                var c2 = result.data[i]['c2']//D1
-                var c3 = result.data[i]['c3']//D10
-                var c4 = result.data[i]['c4']//D50
-                var c5 = result.data[i]['c5']//D90
-                var c6 = result.data[i]['c6']//D99
-                var c7 = result.data[i]['c7']//筛上物
-                var c8 = result.data[i]['c8']//Fe
-                var c9 = result.data[i]['c9']//Ni
-                var c10 = result.data[i]['c10']//Cr
-                var c11 = result.data[i]['c11']//Zn
-                var c12 = result.data[i]['c12']//总量
-                var c13 = result.data[i]['c13']//LiCo3
-                var c14 = result.data[i]['c14']//Na
-                var c15 = result.data[i]['c15']//Mg
-                var c16 = result.data[i]['c16']//Ca
-                var c17 = result.data[i]['c17']//Fe
+                var furnaceNum = result.data[i]['furnaceNum']
+                var pc1 = result.data[i]['pc1']//SSA
+                var pc2 = result.data[i]['pc2']//扣电
+                var pc3 = result.data[i]['pc3']//XRD
 
                 Tcode.append("<tr>"+
                     "<td>"+ operation +"</td>"+
                     "<td>"+ publisher +"</td>"+
                     "<td>"+ auditDate +"</td>"+
                     "<td>"+ batchNumber +"</td>"+
-                    "<td>"+ productDate +"</td>"+
-                    "<td>"+ judge +"</td>"+
-                    "<td>"+ number +"</td>"+
-                    "<td>"+ c1 +"</td>"+
-                    "<td>"+ c2 +"</td>"+
-                    "<td>"+ c3 +"</td>"+
-                    "<td>"+ c4 +"</td>"+
-                    "<td>"+ c5 +"</td>"+
-                    "<td>"+ c6 +"</td>"+
-                    "<td>"+ c7 +"</td>"+
-                    "<td>"+ c8 +"</td>"+
-                    "<td>"+ c9 +"</td>"+
-                    "<td>"+ c10 +"</td>"+
-                    "<td>"+ c11 +"</td>"+
-                    "<td>"+ c12 +"</td>"+
-                    "<td>"+ c13 +"</td>"+
-                    "<td>"+ c14 +"</td>"+
-                    "<td>"+ c15 +"</td>"+
-                    "<td>"+ c16 +"</td>"+
-                    "<td>"+ c17 +"</td>"+
+                    "<td>"+ furnaceNum +"</td>"+
+                    "<td>"+ pc1 +"</td>"+
+                    "<td>"+ pc2 +"</td>"+
+                    "<td>"+ pc3 +"</td>"+
                     "</tr>"
                 )
             }
-        },
+         },
 
+         /** 产品添加Tbody */
+         bindProduct:function(result,Tcode){
+            var length = result.data['length']
+            console.log(length)
+            for(var i=0;i<length;i++){
+                var auditDate = result.data[i]['auditDate']
+                var batchNumber = result.data[i]['batchNumber']
+                var judge = result.data[i]['judge']?result.data[i]['judge']['name']:'null'
+                var number = result.data[i]['number'] 
+                var p1 = result.data[i]['p1']//振实密度
+                var p2 = result.data[i]['p2']//水分
+                var p3 = result.data[i]['p3']//SSA
+                var p4 = result.data[i]['p4']//ph
+                var p5 = result.data[i]['p5']//Lico3
+                var p6 = result.data[i]['p6']//LiOH
+                var p7 = result.data[i]['p7']//总LI含量
+                var p8 = result.data[i]['p8']//D1
+                var p9 = result.data[i]['p9']//D10
+                var p10 = result.data[i]['p10']//D50
+                var p11 = result.data[i]['p11']//D90
+                var p12 = result.data[i]['p12']//D99
+                var p13 = result.data[i]['p13']//颗粒度系数
+                var p14 = result.data[i]['p14']//Fe
+                var p15 = result.data[i]['p15']//Ni
+                var p16 = result.data[i]['p16']//Cr
+                var p17 = result.data[i]['p17']//Zn
+                var p18 = result.data[i]['p18']//总量
+                var p19 = result.data[i]['p19']//Co
+                var p20 = result.data[i]['p20']//Mn
+                var p21 = result.data[i]['p21']//Ni
+                var p22 = result.data[i]['p22']//Li
+                var p23 = result.data[i]['p23']//Co
+                var p24 = result.data[i]['p24']//Mn
+                var p25 = result.data[i]['p25']//Ni
+                var p26 = result.data[i]['p26']//Na
+                var p27 = result.data[i]['p27']//Mg
+                var p28 = result.data[i]['p28']//Ca
+                var p29 = result.data[i]['p29']//Fe
+                var p30 = result.data[i]['p30']//Cu
+                var p31 = result.data[i]['p31']//Zn
+                var p32 = result.data[i]['p32']//S
+                var p33 = result.data[i]['p33']//Al
+                var p34 = result.data[i]['p34']//0.1C首次放电容量
+                var p35 = result.data[i]['p35']//0.1C首次效率
+                var p36 = result.data[i]['p36']//1C首次放电容量
 
-        /** 金弛622添加Tbody */
+                Tcode.append("<tr>"+
+                    "<td>"+ auditDate +"</td>"+
+                    "<td>"+ batchNumber +"</td>"+
+                    "<td>"+ judge +"</td>"+
+                    "<td>"+ number +"</td>"+
+                    "<td>"+ p1 +"</td>"+
+                    "<td>"+ p2 +"</td>"+
+                    "<td>"+ p3 +"</td>"+
+                    "<td>"+ p4 +"</td>"+
+                    "<td>"+ p5 +"</td>"+
+                    "<td>"+ p6 +"</td>"+
+                    "<td>"+ p7 +"</td>"+
+                    "<td>"+ p8 +"</td>"+
+                    "<td>"+ p9 +"</td>"+
+                    "<td>"+ p10 +"</td>"+
+                    "<td>"+ p11 +"</td>"+
+                    "<td>"+ p12 +"</td>"+
+                    "<td>"+ p13 +"</td>"+
+                    "<td>"+ p14 +"</td>"+
+                    "<td>"+ p15 +"</td>"+
+                    "<td>"+ p16 +"</td>"+
+                    "<td>"+ p17 +"</td>"+
+                    "<td>"+ p18 +"</td>"+
+                    "<td>"+ p19 +"</td>"+
+                    "<td>"+ p20 +"</td>"+
+                    "<td>"+ p21 +"</td>"+
+                    "<td>"+ p22 +"</td>"+
+                    "<td>"+ p23 +"</td>"+
+                    "<td>"+ p24 +"</td>"+
+                    "<td>"+ p25 +"</td>"+
+                    "<td>"+ p26 +"</td>"+
+                    "<td>"+ p27 +"</td>"+
+                    "<td>"+ p28 +"</td>"+
+                    "<td>"+ p29 +"</td>"+
+                    "<td>"+ p30 +"</td>"+
+                    "<td>"+ p31 +"</td>"+
+                    "<td>"+ p32 +"</td>"+
+                    "<td>"+ p33 +"</td>"+
+                    "<td>"+ p34 +"</td>"+
+                    "<td>"+ p35 +"</td>"+
+                    "<td>"+ p36 +"</td>"+
+                    "</tr>"
+                )
+            }
+         },
+
+           /** 金弛622添加Tbody */
         bindJinchi622:function(result,Tcode){
             var length = result.data['length']
             console.log(length)
@@ -238,6 +302,146 @@ var inputData_manage = {
 
             }
         },
+
+         /** 制程总锂添加Tbody */
+         bindZhiChengZongli:function(result,Tcode){
+            var length = result.data['length']
+            console.log(length)
+            for(var i=0;i<length;i++){
+                var operation = result.data[i]['operation']
+                var publisher = result.data[i]['publisher']
+                var auditDate = result.data[i]['auditDate']
+                var batchNumber = result.data[i]['batchNumber']
+                var pc1 = result.data[i]['c1']//Lico3
+                var pc2 = result.data[i]['pc2']//LiOH
+                var pc3 = result.data[i]['pc3']//总LI含量
+
+                Tcode.append("<tr>"+
+                    "<td>"+ operation +"</td>"+
+                    "<td>"+ publisher +"</td>"+
+                    "<td>"+ auditDate +"</td>"+
+                    "<td>"+ batchNumber +"</td>"+
+                    "<td>"+ pc1 +"</td>"+
+                    "<td>"+ pc2 +"</td>"+
+                    "<td>"+ pc3 +"</td>"+
+                    "<td>"+ auditDate +"</td>"+
+                    "<td>"+ batchNumber +"</td>"+
+                    "<td>"+ pc1 +"</td>"+
+                    "<td>"+ pc2 +"</td>"+
+                    "<td>"+ pc3 +"</td>"+
+                    "</tr>"
+                )
+
+            }
+         },
+
+         /** 制程粒度添加Tbody */
+         bindZhiChengLidu:function(result,Tcode){
+            var length = result.data['length']
+            console.log(length)
+            for(var i=0;i<length;i++){
+                var operation = result.data[i]['operation']
+                var publisher = result.data[i]['publisher']
+                var auditDate = result.data[i]['auditDate']
+                var batchNumber = result.data[i]['batchNumber']
+                var furnaceNum = result.data[i]['furnaceNum']
+                var pc1 = result.data[i]['pc1']//D0
+                var pc2 = result.data[i]['pc2']//D1
+                var pc3 = result.data[i]['pc3']//D5
+                var pc4 = result.data[i]['pc4']//D10
+                var pc5 = result.data[i]['pc5']//D50
+                var pc6 = result.data[i]['pc6']//D90
+                var pc7 = result.data[i]['pc7']//D95
+                var pc8 = result.data[i]['pc8']//D99
+                var pc9 = result.data[i]['pc9']//D99.99
+                var pc10 = result.data[i]['pc10']//宽度系数
+                var pc10 = result.data[i]['pc10']//宽度系数
+
+                Tcode.append("<tr>"+
+                    "<td>"+ operation +"</td>"+
+                    "<td>"+ publisher +"</td>"+
+                    "<td>"+ auditDate +"</td>"+
+                    "<td>"+ batchNumber +"</td>"+
+                    "<td>"+ furnaceNum +"</td>"+
+                    "<td>"+ pc1 +"</td>"+
+                    "<td>"+ pc2 +"</td>"+
+                    "<td>"+ pc3 +"</td>"+
+                    "<td>"+ pc4 +"</td>"+
+                    "<td>"+ pc5 +"</td>"+
+                    "<td>"+ pc6 +"</td>"+
+                    "<td>"+ pc7 +"</td>"+
+                    "<td>"+ pc8 +"</td>"+
+                    "<td>"+ pc9 +"</td>"+
+                    "<td>"+ pc10 +"</td>"+
+                    "<td>"+ pc10 +"</td>"+
+                    "</td>"
+                )
+
+            }
+         },
+
+        /** 天齐碳酸锂添加Tbody */
+        bindTianQi:function(result,Tcode){
+            var length = result.data['length']
+            console.log(length)
+            for(var i=0;i<length;i++){
+                var operation = result.data[i]['operation']
+                var publisher = result.data[i]['publisher']
+                var auditDate = result.data[i]['auditDate']
+                var batchNumber = result.data[i]['batchNumber']
+                var productDate = result.data[i]['productDate'] 
+                var judge = result.data[i]['judge']?result.data[i]['judge']['name']:'null'
+                var number = result.data[i]['number'] 
+                var c1 = result.data[i]['c1']//水分
+                var c2 = result.data[i]['c2']//D1
+                var c3 = result.data[i]['c3']//D10
+                var c4 = result.data[i]['c4']//D50
+                var c5 = result.data[i]['c5']//D90
+                var c6 = result.data[i]['c6']//D99
+                var c7 = result.data[i]['c7']//筛上物
+                var c8 = result.data[i]['c8']//Fe
+                var c9 = result.data[i]['c9']//Ni
+                var c10 = result.data[i]['c10']//Cr
+                var c11 = result.data[i]['c11']//Zn
+                var c12 = result.data[i]['c12']//总量
+                var c13 = result.data[i]['c13']//LiCo3
+                var c14 = result.data[i]['c14']//Na
+                var c15 = result.data[i]['c15']//Mg
+                var c16 = result.data[i]['c16']//Ca
+                var c17 = result.data[i]['c17']//Fe
+
+                Tcode.append("<tr>"+
+                    "<td>"+ operation +"</td>"+
+                    "<td>"+ publisher +"</td>"+
+                    "<td>"+ auditDate +"</td>"+
+                    "<td>"+ batchNumber +"</td>"+
+                    "<td>"+ productDate +"</td>"+
+                    "<td>"+ judge +"</td>"+
+                    "<td>"+ number +"</td>"+
+                    "<td>"+ c1 +"</td>"+
+                    "<td>"+ c2 +"</td>"+
+                    "<td>"+ c3 +"</td>"+
+                    "<td>"+ c4 +"</td>"+
+                    "<td>"+ c5 +"</td>"+
+                    "<td>"+ c6 +"</td>"+
+                    "<td>"+ c7 +"</td>"+
+                    "<td>"+ c8 +"</td>"+
+                    "<td>"+ c9 +"</td>"+
+                    "<td>"+ c10 +"</td>"+
+                    "<td>"+ c11 +"</td>"+
+                    "<td>"+ c12 +"</td>"+
+                    "<td>"+ c13 +"</td>"+
+                    "<td>"+ c14 +"</td>"+
+                    "<td>"+ c15 +"</td>"+
+                    "<td>"+ c16 +"</td>"+
+                    "<td>"+ c17 +"</td>"+
+                    "</tr>"
+                )
+            }
+        },
+
+
+      
 
          /** 监听下拉菜单的option */
          bindCreatoption:function(){
