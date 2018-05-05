f04d4ea6fdc74925503fb89b1cae523a00e5e477
lanyage <lanyage520@foxmail.com>
2018-05-05 16:41:25 +0800

    修改

--- components/js/mat_out_manage.js
+++ components/js/mat_out_manage.js
@@ -1,8 +1,9 @@
 var mat_out_manage = {
     pageSize : null,
     init: function () {
+        /** 渲染表格 */
         mat_out_manage.funcs.renderTable()
-        mat_out_manage.funcs.checkboxEventBinding()
+        // mat_out_manage.funcs.checkboxEventBinding()
         /** 将分页居中 */
         var out = $('#material_out_page').width()
         var time = setTimeout(function () {
@@ -12,15 +13,28 @@ var mat_out_manage = {
         }, 30)
     },
     funcs: {
+
         renderTable: function () {
+            // result {
+            //     code : 0/-1,
+            //         data : user,
+            //         message : '登录成功/登录失败'
+            // }
+            // $.get("http://localhost:8080/mes/user/login",{username:'lanyage', password : '123'},function(res) {
+            //     if()
+            // })
+
             $.post(home.urls.materialOut.getAllByPage(), {}, function (res) {
+                console.log(res.data.content)
+
                 var $tbody = $("#material_out_table").children('tbody')
+
+                /** 过滤返回的数据 */
                 var items = res.data.content
                 mat_out_manage.funcs.renderHandler($tbody, items)
                 /** 渲染表格结束之后 */
                 mat_out_manage.pageSize = res.data.content.length //该页的记录数
                 var page = res.data //分页json
-                console.log(page.totalPages)
                 /** 分页信息 */
                 layui.laypage.render({
                     elem: 'material_out_page'
@@ -43,23 +57,30 @@ var mat_out_manage = {
             })
         }
         , renderHandler: function ($tbody, items) {
-            // $tbody.empty() //清空表格
-            // <tr>
-            // <td><input type="checkbox" class="mat_out_checkbox"></td>
-            //     <td>领料一部</td>
-            //     <td>2017-1-12</td>
-            //     <td>已审核</td>
-            //     <td>已审核</td>
-            //     <td>领料状态</td>
-            //     <td><a href="#" class='detail'><i class="layui-icon">&#xe60a;</i></a></td>
-            // </tr>
+            $tbody.empty() //清空表格
             items.forEach(function (e) {
-                console.log(e)
-                //根据当前元素来渲染页面
+                var code = e.code
+                var contact = e.contact
+                var date = e.sendDate
+                var status = e.status
 
+                var content = (
+                    "<tr>"+
+                   " <td>"+(code)+"</td>"+
+                        "<td>"+(contact)+"</td>"+
+                      "  <td>"+(date)+"</td>"+
+                       " <td>"+(status)+"</td>"+
+                        "<td>"+(status)+"</td>"+
+                        "<td>"+(status)+"</td>"+
+                        "<td><a href=\"#\" class='detail' id='detail-"+(code)+"'><i class=\"layui-icon\">&#xe60a;</i></a></td>"+
+                    "</tr>"
+                )
+                $tbody.append(content)
             })
-            /** 绑定全选事件 */
-            mat_out_manage.funcs.checkboxEventBinding()
+
+
+            // /** 绑定全选事件 */
+            // mat_out_manage.funcs.checkboxEventBinding()
             /** 数据渲染完毕之后,需要进行绑定详情点击按钮事件 */
             var detailBtns = $(".detail")
             mat_out_manage.funcs.bindDetailClick(detailBtns)
@@ -69,7 +90,7 @@ var mat_out_manage = {
                 //点击的时候需要弹出一个模态框
                 layer.open({
                     type: 1,
-                    title: '原料入库单',
+                    title: '原料出库单',
                     content: "<div><h1>Hello World!</h1></div>",
                     area: ['350px', '200px'],
                     btn: ['打印', '返回'],
