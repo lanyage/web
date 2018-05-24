72d6ffa1f2dfa1bcb90c6b5faaa6e2c76b744ed0
lanyage <lanyage520@foxmail.com>
2018-05-24 14:40:15 +0800

    fix bugs

--- components/js/mat_in_manage.js
+++ components/js/mat_in_manage.js
@@ -11,7 +11,7 @@ var mat_in_manage = {
             $.post(home.urls.materialIn.getAllByPage(), {}, function (res) {
                 var $tbody = $("#material_in_table").children('tbody')
                 var items = res.data.content
-               
+
                 mat_in_manage.funcs.renderHandler($tbody, items)
                 /** 渲染表格结束之后 */
                 mat_in_manage.pageSize = res.data.content.length //该页的记录数
@@ -38,24 +38,19 @@ var mat_in_manage = {
                 })
                 $('#material_in_page').css('padding-left', '37%')
             })
+            // 追加刷新事件
+            var refreshBtn = $('#model-li-hide-refresh-48');
+            mat_in_manage.funcs.bindRefreshEventListener(refreshBtn);//追加刷新事件
 
+            /** 追加搜索事件 */
+            var searchBtn = $('#model-li-hide-search-48');
+            mat_in_manage.funcs.bindSearchEventListener(searchBtn);
 
-
-             // 追加刷新事件
-             var refreshBtn = $('#model-li-hide-refresh-48');
-             mat_in_manage.funcs.bindRefreshEventListener(refreshBtn);//追加刷新事件
-
-             /** 追加搜索事件 */
-           var searchBtn = $('#model-li-hide-search-48');
-           mat_in_manage.funcs.bindSearchEventListener(searchBtn);
-       
         }
         , renderHandler: function ($tbody, items) {
             $tbody.empty() //清空表格
-            console.log(items)
             items.forEach(function (e) {
                 // $('#dep_checkAll').prop('checked', false)
-                console.log(e.code)
                 var content = (
                     "<tr>" +
                     "<td>" + (e.code) + "</td>" +
@@ -75,8 +70,8 @@ var mat_in_manage = {
             var detailBtns = $(".detail")
             mat_in_manage.funcs.bindDetailClick(detailBtns)
         }
-       /**获取搜索的数据 */
-      /**  , renderHandler1: function ($tbody, items,supplierCode) {
+        /**获取搜索的数据 */
+        /**  , renderHandler1: function ($tbody, items,supplierCode) {
             $tbody.empty() //清空表格
             console.log(items)
             items.forEach(function (e) {
@@ -99,86 +94,77 @@ var mat_in_manage = {
             })    var detailBtns = $(".detail")
             mat_in_manage.funcs.bindDetailClick(detailBtns)
             // /** 绑定全选事件 */
-            // mat_in_manage.funcs.checkboxEventBinding()
-            /** 数据渲染完毕之后,需要进行绑定详情点击按钮事件 */
-        
-    
+        /** 数据渲染完毕之后,需要进行绑定详情点击按钮事件 */
 
 
-        ,getSupplier: function(){
+        , getSupplier: function () {
             $.get(home.urls.materialIn.getAllSupplier(), {}, function (res) {
-            select = $("#model-li-select-48")
-            items = res.data
-          //  console.log(items)
-            select.html("<option>选择生产厂家</option>");
-            items.forEach(function (e) {
-                
-                select.append(
-                    "<option value="+e.code+">"+(e.name)+"</option>" 
-                )
-                console.log(e)
-            })     
-        })
+                select = $("#model-li-select-48")
+                items = res.data
+                //  console.log(items)
+                select.html("<option>选择生产厂家</option>");
+                items.forEach(function (e) {
+                    select.append(
+                        "<option value=" + e.code + ">" + (e.name) + "</option>"
+                    )
+                })
+            })
         }
 
-        ,fillData: function(table,items) {
-            
-            console.log(items)
+        , fillData: function (table, items) {
             $("#batchNumber").text(items.batchNumber)
             $("#name").text(items.name)
-            $("#productor").text(!items.supplier?null:items.supplier.name)
-            $("#number").text(!items.number?'无':items.number)
+            $("#productor").text(!items.supplier ? null : items.supplier.name)
+            $("#number").text(!items.number ? '无' : items.number)
             $("#date").text(items.date)
-            $("#creatUser").text(items.createUser?items.createUser.name:'无')
+            $("#creatUser").text(items.createUser ? items.createUser.name : '无')
             $("#creatTime").text(new Date().Format("yyyy-MM-dd hh:mm:ss"))
             var godownEntries = items.godownEntries
             var $tbody = $('#down_table').children('tbody')
             $tbody.empty() //清空表格
-            godownEntries.forEach(function(ele) {
-                console.log(ele)
+            godownEntries.forEach(function (ele) {
                 $tbody.append(
-                    "<tr>"+
-                    " <td>"+(ele.code)+"</td>"+
-                    "<td>"+(ele.batchNumber)+"</td>"+
-                    "<td>"+(!ele.unit?'kg':ele.unit)+"</td>"+
-                    "<td>"+(!ele.weight?0:ele.weight)+"</td>"
+                    "<tr>" +
+                    " <td>" + (ele.code) + "</td>" +
+                    "<td>" + (ele.batchNumber) + "</td>" +
+                    "<td>" + (!ele.unit ? 'kg' : ele.unit) + "</td>" +
+                    "<td>" + (!ele.weight ? 0 : ele.weight) + "</td>"
                 )
-            }) 
+            })
         }
         , bindDetailClick: function (detailBtns) {
             detailBtns.off('click').on('click', function () {
-                console.log($(this).attr('id'))
                 var _selfBtn = $(this)
                 var codeNumber = _selfBtn.attr('id').substr(7)
                 $.post(home.urls.materialIn.getAllByCode(), {
                     code: codeNumber
-                   
+
                 }, function (result) {
                     var items = result.data//获取数据
-                //点击的时候需要弹出一个模态框
-                mat_in_manage.funcs.fillData($("#detail_modal"),items)  //将获取的数据传到#detail_modal中
-                layer.open({
-                    type: 1,
-                    title: '原料入库单',
-                    content: $("#detail_modal"),
-                    area: ['800px', '430px'],
-                    btn: ['打印', '返回'],
-                    offset: "auto",
-                    closeBtn:0,
-                    yes: function (index) {
-                        //点击确定之后必须打印当前表单,推荐第三方插件 printthis.js todo
-                        $("#detail_modal").css('display', 'none')
-                        layer.close(index)
-                    },
-                    btn2: function (index) {
-                        $("#detail_modal").css('display', 'none')
-                        layer.close(index)
-                    }
+                    //点击的时候需要弹出一个模态框
+                    mat_in_manage.funcs.fillData($("#detail_modal"), items)  //将获取的数据传到#detail_modal中
+                    layer.open({
+                        type: 1,
+                        title: '原料入库单',
+                        content: $("#detail_modal"),
+                        area: ['800px', '430px'],
+                        btn: ['打印', '返回'],
+                        offset: "auto",
+                        closeBtn: 0,
+                        yes: function (index) {
+                            //点击确定之后必须打印当前表单,推荐第三方插件 printthis.js todo
+                            $("#detail_modal").css('display', 'none')
+                            layer.close(index)
+                        },
+                        btn2: function (index) {
+                            $("#detail_modal").css('display', 'none')
+                            layer.close(index)
+                        }
+                    })
                 })
             })
-        })
-    }
-        
+        }
+
         /** 全选逻辑 */
         , checkboxEventBinding: function () {
             var selectAllBox = $('#mat_in_checkAll')
@@ -208,41 +194,39 @@ var mat_in_manage = {
         }
         /** $全选逻辑结束$ */
 
-         /** $全选逻辑结束$ */
-             /**
+        /** $全选逻辑结束$ */
+        /**
          * 刷新事件-已完成
          * @param refreshBtn
          */
-        ,bindRefreshEventListener: function (refreshBtn) {
+        , bindRefreshEventListener: function (refreshBtn) {
             refreshBtn.off('click')
-            refreshBtn.on('click', function () { 
-                
+            refreshBtn.on('click', function () {
+
                 var index = layer.load(2, {offset: ['40%', '58%']});
                 var time = setTimeout(function () {
                     layer.msg('刷新成功', {
                         offset: ['40%', '55%'],
                         time: 700
-                    }) 
+                    })
                     mat_in_manage.init()
                     layer.close(index)
                     clearTimeout(time)
                 }, 200)
-               
+
             })
         }
-   
-          /** 搜索事件 */
-          ,bindSearchEventListener: function (searchBtn) {
+
+        /** 搜索事件 */
+        , bindSearchEventListener: function (searchBtn) {
             searchBtn.off('click')
             searchBtn.on('click', function () {
                 var code = $('#model-li-select-48').val()
-                console.log(code)
                 $.post(home.urls.materialIn.getBySupplierByPage(), {
                     supplierCode: code,
                 }, function (result) {
                     var items = result.data.content //获取数据
-                    page = result.data 
-                    console.log(items)
+                    page = result.data
                     var code = $('#model-li-select-48').val()
                     const $tbody = $("#material_in_table").children('tbody')
                     mat_in_manage.funcs.renderHandler($tbody, items)
@@ -257,7 +241,7 @@ var mat_in_manage = {
                                     size: obj.limit
                                 }, function (result) {
                                     var items = result.data.content //获取数据
-                                   // var code = $('#model-li-select-48').val()
+                                    // var code = $('#model-li-select-48').val()
                                     const $tbody = $("#material_in_table").children('tbody')
                                     mat_in_manage.funcs.renderHandler($tbody, items)
                                     mat_in_manage.pageSize = result.data.content.length
--- components/js/product_in_manage.js
+++ components/js/product_in_manage.js
@@ -3,7 +3,7 @@ var product_in_manage = {
     init: function () {
         /** 渲染表格 */
         product_in_manage.funcs.renderTable()
-       
+
         /** 需要给刷新按钮和搜索按钮绑定点击事件 */
         /** 将分页居中 */
         var out = $('#product_in_page').width()
@@ -12,180 +12,163 @@ var product_in_manage = {
             $('#product_in_page').css('padding-left', 100 * ((out - inside) / 2 / out) > 33 ? 100 * ((out - inside) / 2 / out) + '%' : '35.5%')
             clearTimeout(time)
         }, 30)
-    
+
     },
 
-funcs: {
-    renderTable: function () {
-        $.post(home.urls.productIn.getAllByPage(), {}, function (res) {
-            var $tbody = $("#product_in_table").children('tbody')
-            var items = res.data.content
-            console.log(items)
-            product_in_manage.funcs.renderHandler($tbody, items)
-            /** 渲染表格结束之后 */
-            product_in_manage.pageSize = res.data.content.length //该页的记录数
-            var page = res.data //分页json
-            /** 分页信息 */
-            layui.laypage.render({
-                elem: 'product_in_page'
-                , count: 10 * page.totalPages//数据总数
-                /** 页面变化后的逻辑 */
-                , jump: function (obj, first) {
-                    if (!first) {
-                        console.log('不是首次,可以执行')
-                        $.post(home.urls.productIn.getAllByPage(), {
-                            page: obj.curr - 1,
-                            size: obj.limit
-                        }, function (result) {
-                            var items = result.data.content //获取数据
-                            const $tbody = $("#product_in_table").children('tbody')
-                            product_in_manage.funcs.renderHandler($tbody, items)
-                            product_in_manage.pageSize = result.data.content.length
-                        })
+    funcs: {
+        renderTable: function () {
+            $.post(home.urls.productIn.getAllByPage(), {}, function (res) {
+                var $tbody = $("#product_in_table").children('tbody')
+                var items = res.data.content
+                product_in_manage.funcs.renderHandler($tbody, items)
+                /** 渲染表格结束之后 */
+                product_in_manage.pageSize = res.data.content.length //该页的记录数
+                var page = res.data //分页json
+                /** 分页信息 */
+                layui.laypage.render({
+                    elem: 'product_in_page'
+                    , count: 10 * page.totalPages//数据总数
+                    /** 页面变化后的逻辑 */
+                    , jump: function (obj, first) {
+                        if (!first) {
+                            console.log('不是首次,可以执行')
+                            $.post(home.urls.productIn.getAllByPage(), {
+                                page: obj.curr - 1,
+                                size: obj.limit
+                            }, function (result) {
+                                var items = result.data.content //获取数据
+                                const $tbody = $("#product_in_table").children('tbody')
+                                product_in_manage.funcs.renderHandler($tbody, items)
+                                product_in_manage.pageSize = result.data.content.length
+                            })
+                        }
                     }
-                }
-            })
-            $('#product_in_page').css('padding-left', '37%')
-        })
-
-         // 追加刷新事件
-
-         var refreshBtn = $('#model-li-hide-refresh-49');
-         product_in_manage.funcs.bindRefreshEventListener(refreshBtn);
-
-       /**   追加搜索事件*/
-       var searchBtn = $('#model-li-hide-search-49');
-       product_in_manage.funcs.bindSearchEventListener(searchBtn); 
-   
-    },
-
-    renderHandler: function ($tbody, items) {
-        $tbody.empty() //清空表格
-        console.log(items)
-        items.forEach(function (e) {
-            // $('#dep_checkAll').prop('checked', false)
-            console.log(e.code)
-            var content = (
-                "<tr>" +
-                "<td>" + (e.code) + "</td>" +
-                "<td>" + (e.batchNumber) + "</td>" +
-                "<td>" + (e.department ? e.department.name : null) + "</td>" +
-                "<td>" + (e.payTime) + "</td>" +
-                "<td>" + (e.status) + "</td>" +
-                "<td><a href='#' class='detail' id='detail-" + (e.code) + "'><i class='layui-icon'>&#xe60a;</i></a></td>" +
-                "</tr>"
-            )
-            $tbody.append(content)
-        })
-        // /** 绑定全选事件 */
-        // mat_in_manage.funcs.checkboxEventBinding()
-        /** 数据渲染完毕之后,需要进行绑定详情点击按钮事件 */
-        var detailBtns = $(".detail")
-        product_in_manage.funcs.bindDetailClick(detailBtns)
-    },
-
-     bindDetailClick: function (detailBtns) {
-        detailBtns.off('click').on('click', function () {
-            console.log(1)
-         console.log($(this).attr('id'))
-           var _selfBtn = $(this)
-           var codeNumber = _selfBtn.attr('id').substr(7)
-         $.post(home.urls.productIn.getByCode(),{
-               code:codeNumber
-           },function(result) {
-               var items = result.data  //获取数  /** */
-               console.log(items)
-            //点击的时候需要弹出一个模态框
-            product_in_manage.funcs.fillData($("#detail_modal"),items)  //将获取的数据传到#detail_modal中
-            layer.open({
-                type: 1,
-                title: '成品入库详情',
-                content: $("#detail_modal"),
-                area: ['800px', '430px'],
-                btn: [' 返回'],
-                offset: "auto",
-                closeBtn:0,
-                yes: function (index) {
-                    //点击确定之后必须打印当前表单,推荐第三方插件 printthis.js todo
-                    $("#detail_modal").css('display', 'none')
-                    layer.close(index)
-                },
-                btn2: function (index) {
-                    $("#detail_modal").css('display', 'none')
-                    layer.close(index)
-                }
-            })
-        })
-        })
-  
-       },
-
-    
-    /** 刷新事件 */
-    bindRefreshEventListener: function (refreshBtn) {
-        refreshBtn.off('click')
-        refreshBtn.on('click', function () {
-            console.log('AAAAAAAAAAAAAAAAAAAAAAaa')
-            var index = layer.load(2, {offset: ['40%', '58%']});
-            var time = setTimeout(function () {
-                layer.msg('刷新成功', {
-                    offset: ['40%', '55%'],
-                    time: 700
                 })
-                product_in_manage.init()
-                layer.close(index)
-                clearTimeout(time)
-            }, 200)
-        })
-    },
+                $('#product_in_page').css('padding-left', '37%')
+            })
 
-    fillData: function(table,items) {
-            
-        //  console.log(items)
-          $("#batchNumber").text(items.batchNumber?items.batchNumber:'null')
-          $("#model").text(items.model?items.model:'null')
-          $("#department").text(items.department?items.department.name:'null')
-          $("#weight").text(items.weight?items.weight:'kg')
-          $("#payer").text(items.payer?items.payer.name:'null')
-          $("#godowner").text(items.godowner?items.godowner:'null')
-          $("#payTime").text(items.payTime?items.payTime:'null')
-          $("#godownTime").text(items.godownTime?items.godownTime:'null')
-         /** $("#batchNumber").text('12345')
-          $("#model").text('1')
-          $("#department").text('部门')
-          $("#weight").text('22')
-          $("#payer").text('张安')
-          $("#godowner").text('李四')
-          $("#payTime").text('2017-10-20')
-          $("#godownTime").text('2018-01-01')*/
-          var productGodowns = items.productGodowns 
-          var $tbody = $('#down_table').children('tbody')
-          $tbody.empty() //清空表格
-          productGodowns.forEach(function(ele) {
-           //   console.log(ele)
-              $tbody.append(
-                  "<tr>"+
-                  " <td>"+(ele.code)+"</td>"+
-                  "<td>"+(ele.batchNumber)+"</td>"+
-                  "<td>"+(!ele.unit?'kg':ele.unit)+"</td>"+
-                  "<td>"+(!ele.weight?0:ele.weight)+"</td>"+
-                  " <td>"+(ele.status)+"</td>"
-              )
-          }) 
-      },
+            // 追加刷新事件
 
-          /** 搜索事件 */
-          bindSearchEventListener: function (searchBtn) {
+            var refreshBtn = $('#model-li-hide-refresh-49');
+            product_in_manage.funcs.bindRefreshEventListener(refreshBtn);
+
+            /**   追加搜索事件*/
+            var searchBtn = $('#model-li-hide-search-49');
+            product_in_manage.funcs.bindSearchEventListener(searchBtn);
+
+        },
+
+        renderHandler: function ($tbody, items) {
+            $tbody.empty() //清空表格
+            items.forEach(function (e) {
+                var content = (
+                    "<tr>" +
+                    "<td>" + (e.code) + "</td>" +
+                    "<td>" + (e.batchNumber) + "</td>" +
+                    "<td>" + (e.department ? e.department.name : null) + "</td>" +
+                    "<td>" + (e.payTime) + "</td>" +
+                    "<td>" + (e.status) + "</td>" +
+                    "<td><a href='#' class='detail' id='detail-" + (e.code) + "'><i class='layui-icon'>&#xe60a;</i></a></td>" +
+                    "</tr>"
+                )
+                $tbody.append(content)
+            })
+            // /** 绑定全选事件 */
+            // mat_in_manage.funcs.checkboxEventBinding()
+            /** 数据渲染完毕之后,需要进行绑定详情点击按钮事件 */
+            var detailBtns = $(".detail")
+            product_in_manage.funcs.bindDetailClick(detailBtns)
+        },
+
+        bindDetailClick: function (detailBtns) {
+            detailBtns.off('click').on('click', function () {
+                var _selfBtn = $(this)
+                var codeNumber = _selfBtn.attr('id').substr(7)
+                $.post(home.urls.productIn.getByCode(), {
+                    code: codeNumber
+                }, function (result) {
+                    var items = result.data  //获取数  /** */
+                    // console.log(items)
+                    //点击的时候需要弹出一个模态框
+                    product_in_manage.funcs.fillData($("#detail_modal"), items)  //将获取的数据传到#detail_modal中
+                    layer.open({
+                        type: 1,
+                        title: '成品入库详情',
+                        content: $("#detail_modal"),
+                        area: ['800px', '430px'],
+                        btn: [' 返回'],
+                        offset: "auto",
+                        closeBtn: 0,
+                        yes: function (index) {
+                            //点击确定之后必须打印当前表单,推荐第三方插件 printthis.js todo
+                            $("#detail_modal").css('display', 'none')
+                            layer.close(index)
+                        },
+                        btn2: function (index) {
+                            $("#detail_modal").css('display', 'none')
+                            layer.close(index)
+                        }
+                    })
+                })
+            })
+
+        },
+
+
+        /** 刷新事件 */
+        bindRefreshEventListener: function (refreshBtn) {
+            refreshBtn.off('click')
+            refreshBtn.on('click', function () {
+                console.log('AAAAAAAAAAAAAAAAAAAAAAaa')
+                var index = layer.load(2, {offset: ['40%', '58%']});
+                var time = setTimeout(function () {
+                    layer.msg('刷新成功', {
+                        offset: ['40%', '55%'],
+                        time: 700
+                    })
+                    product_in_manage.init()
+                    layer.close(index)
+                    clearTimeout(time)
+                }, 200)
+            })
+        },
+
+        fillData: function (table, items) {
+            //  console.log(items)
+            $("#batchNumber").text(items.batchNumber ? items.batchNumber : 'null')
+            $("#model").text(items.model ? items.model : 'null')
+            $("#department").text(items.department ? items.department.name : 'null')
+            $("#detail_weight").text(items.weight ? items.weight : 'kg')
+            $("#payer").text(items.payer ? items.payer.name : 'null')
+            $("#godowner").text(items.godowner ? items.godowner : 'null')
+            $("#payTime").text(items.payTime ? items.payTime : 'null')
+            $("#godownTime").text(items.godownTime ? items.godownTime : 'null')
+            var productGodowns = items.productGodowns
+            var $tbody = $('#down_table').children('tbody')
+            $tbody.empty() //清空表格
+            productGodowns.forEach(function (ele) {
+                $tbody.append(
+                    "<tr>" +
+                    " <td>" + (ele.code) + "</td>" +
+                    "<td>" + (ele.batchNumber) + "</td>" +
+                    "<td>" + (!ele.unit ? 'kg' : ele.unit) + "</td>" +
+                    "<td>" + (!ele.weight ? 0 : ele.weight) + "</td>" +
+                    " <td>" + (ele.status) + "</td>"
+                )
+            })
+        },
+
+        /** 搜索事件 */
+        bindSearchEventListener: function (searchBtn) {
             searchBtn.off('click')
             searchBtn.on('click', function () {
                 var statusVal = $('#model-li-hide-select-49').val()
-                console.log(statusVal)
                 $.post(home.urls.productIn.getByStatusByPage(), {
                     status: statusVal,
                 }, function (result) {
                     var page = result.data
                     var items = result.data.content //获取数据
-                 //   console.log(items)
+                    //   console.log(items)
                     var code = $('#model-li-hide-select-49').val()
                     const $tbody = $("#product_in_table").children('tbody')
                     product_in_manage.funcs.renderHandler($tbody, items)
@@ -200,7 +183,7 @@ funcs: {
                                     size: obj.limit
                                 }, function (result) {
                                     var items = result.data.content //获取数据
-                                   // var code = $('#model-li-select-48').val()
+                                    // var code = $('#model-li-select-48').val()
                                     const $tbody = $("#product_in_table").children('tbody')
                                     product_in_manage.funcs.renderHandler($tbody, items)
                                     product_in_manage.pageSize = result.data.content.length
@@ -212,5 +195,5 @@ funcs: {
             })
         }
 
-}
+    }
 }
