1e15ed669b5e1887c61eb33ffbc5e7384dd4dbcb
WZH9606 <1192573254@qq.com>
2018-06-06 10:32:41 +0800

    盘库预警完成

--- js/home.js
+++ js/home.js
@@ -726,7 +726,10 @@ var home = {
                 return servers.backup() + '/materialsTotal/getAllByPage'
             },
             getByWarnStatusByPage:function(){
-                return servers.backup() +  '/materialsTotal/getByWarmStatusByPage'
+                return servers.backup() +  '/materialsTotal/getByWarnStatusByPage'
+            },
+            getAllWarnStatus(){
+                return　servers.backup() +  '/materialsTotal'
             }
         },
         lingLiao: {
